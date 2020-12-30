680e5e82097aa2d3cd5132e4ece90d5d4187f29e
Achyut Upadhyay <acupadhy@microsoft.com>
2020-12-30 22:08:44 +0530

    Add DbContext & Initial Create Migration

---
+++ WebApi_1/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi_1.Models;
+
+namespace WebApi_1.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Character> Characters { get; set; }     
+    }
+}
--- WebApi_1/Startup.cs
+++ WebApi_1/Startup.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebApi_1.Data;
 using WebApi_1.Services;
 
 namespace WebApi_1
@@ -27,6 +29,7 @@ namespace WebApi_1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
             services.AddScoped<ICharacterService, CharacterService>();
